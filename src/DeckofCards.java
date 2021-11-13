75af26520577df7e5ad4945aceba6d3e1c98baf1
Soumya795 <soumyasubhakanta97@gmail.com>
2021-11-13 19:37:48 +0530

    Added: Add the Players

--- src/DeckofCards.java
+++ src/DeckofCards.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class DeckofCards {
-    public static void main(String[] args) {
+    public static void initGame() {
         int card = 0;
         String[] suit = {"Club", "Diamond", "Spades", "Hearts"};
         String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
@@ -14,5 +16,24 @@ public class DeckofCards {
 
         }
         System.out.println("The total number of cards are " + card);
+    }
+
+    public static void addPlayer() {
+        System.out.println("Enter Number of Players");
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        if (number >= 2 && number <= 4) {
+            System.out.println("Lets start the Game");
+        } else {
+            System.out.println("Game can be played by 2 , 3 and 4 players only ");
+            System.out.println("Please enter again");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Deck of Card Problem");
+        initGame();
+        addPlayer();
+
     }
 }
